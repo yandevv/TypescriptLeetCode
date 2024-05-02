b389ae0c0988c566bbcef916739c79c3de7f7c85
Yan Victor Brito Gomes <ybritogomes@gmail.com>
2024-05-02 18:04:06 -0300

    feat(topSWE): Added another solution to powerOfThree.ts(326) challenge

--- easyChallenges/powerOfThree.ts
+++ easyChallenges/powerOfThree.ts
@@ -4,11 +4,20 @@
 //     return parseInt(`${n * (1/3)}`) === n * (1/3);
 // };
 
+// First try with while and multiplying 3 until match n
+// function isPowerOfThree(n: number): boolean {
+//     let powerNumber = 1;
+//     while(powerNumber <= n) {
+//         if(powerNumber === n) return true;
+//         powerNumber *= 3;
+//     }
+//     return false;
+// };
+
+// Second try with recursion
 function isPowerOfThree(n: number): boolean {
-    let powerNumber = 1;
-    while(powerNumber <= n) {
-        if(powerNumber === n) return true;
-        powerNumber *= 3;
-    }
-    return false;
+    if(n === 1) return true;
+    if(n % 3 !== 0 || n < 1) return false;
+    n = n / 3;
+    return isPowerOfThree(n);
 };
