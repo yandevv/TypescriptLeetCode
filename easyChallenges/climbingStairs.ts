305fcb41f3db2f349949c8f01192077ed63dae04
YanDevv <ybritogomes@gmail.com>
2024-05-03 20:18:51 -0300

    feat(topSWE): Added solved climbingStairs.ts(70) challenge

---
+++ easyChallenges/climbingStairs.ts
@@ -0,0 +1,10 @@
+// Dynamic Programming Approach
+function climbStairs(n: number): number {
+    let dp: number[] = [];
+    dp[0] = 1;
+    dp[1] = 1;
+    for(let i = 2; i <= n; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+    return dp[n];
+};
