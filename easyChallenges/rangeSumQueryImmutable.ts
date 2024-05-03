aace2c8825ef60e4ece4abc927bc5f7e33ad07f7
Yan Victor Brito Gomes <ybritogomes@gmail.com>
2024-05-03 13:39:12 -0300

    feat(topSWE): Added another solution to rangeSumQueryImmutable.ts(303) challenge

--- easyChallenges/rangeSumQueryImmutable.ts
+++ easyChallenges/rangeSumQueryImmutable.ts
@@ -1,3 +1,4 @@
+// First approach using O(1) Time-Complexity on nums storing, and O(n) on index calculation
 class NumArray {
     nums: number[];
     constructor(nums: number[]) {
@@ -10,5 +11,20 @@ class NumArray {
             sum += this.nums[i];
         }
         return sum;
+    }
+}
+
+// Second approach using O(n) Time-Complexity on result storing for prefix sum, and O(1) on index calculation using prefix sum.
+class NumArray {
+    result: number[] = [];
+    constructor(nums: number[]) {
+        this.result[0] = nums[0];
+        for(let i = 1; i < nums.length; i++) {
+            this.result[i] = this.result[i - 1] + nums[i];
+        }
+    }
+
+    sumRange(left: number, right: number): number {
+        return left > 0 ? this.result[right] - this.result[left - 1] : this.result[right];
     }
 }
