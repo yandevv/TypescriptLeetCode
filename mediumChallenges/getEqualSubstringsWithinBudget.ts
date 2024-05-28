8e73c7dda4124b9a4904bd2ed2ec20a47bb1641a
Yan Victor Brito Gomes <ybritogomes@gmail.com>
2024-05-28 12:11:16 -0300

    feat(dailyChallenge): Added another solution to getEqualSubstringsWithinBudget.ts(1208) challenge

--- mediumChallenges/getEqualSubstringsWithinBudget.ts
+++ mediumChallenges/getEqualSubstringsWithinBudget.ts
@@ -40,3 +40,23 @@ function equalSubstring(s: string, t: string, maxCost: number): number {
     }
     return maximumLength;
 };
+
+// Second Approach - O(n) with Sliding Window, iterating all numbers and adjusting the start of window (https://leetcode.com/problems/get-equal-substrings-within-budget/solutions/5218505/97-44-easy-solution-with-explanation/?envType=daily-question&envId=2024-05-28)
+function diffCalc(index: number, s: string, t: string) {
+    return Math.abs(s.charCodeAt(index) - t.charCodeAt(index));
+}
+
+function equalSubstring(s: string, t: string, maxCost: number): number {
+    let maximumLength: number = 0;
+    let i: number = 0;
+    let currCost: number = 0;
+    for(let j: number = 0; j < s.length; j++) {
+        currCost += diffCalc(j, s, t);
+        while(currCost > maxCost) {
+            currCost -= diffCalc(i, s, t);
+            i++;
+        }
+        maximumLength = Math.max(maximumLength, j - i + 1);
+    }
+    return maximumLength;
+};
