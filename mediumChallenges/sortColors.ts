97410f5bc097c9e02ef7c65f2bfc606d1a41d194
Yan Victor Brito Gomes <ybritogomes@gmail.com>
2024-06-17 12:25:31 -0300

    feat(dailyChallenge): Added another solution to sortColors.ts(75) challenge

--- mediumChallenges/sortColors.ts
+++ mediumChallenges/sortColors.ts
@@ -6,3 +6,26 @@
  function sortColors(nums: number[]): void {
     nums.sort((a, b) => a - b);
 };
+
+// Second Approach - Counting sorting through all 0s, 1s and 2s, and assembling array after count - (https://leetcode.com/problems/sort-colors/solutions/5299410/beats-100-00-of-users-with-java-counter-approach-simple-easy-explained-solution/?envType=daily-question&envId=2024-06-12)
+function sortColors(nums: number[]): void {
+    let zeroCounter: number = 0, oneCounter: number = 0, twoCounter: number = 0;
+    for(let num of nums) {
+        if(num === 0) zeroCounter++;
+        if(num === 1) oneCounter++;
+        if(num === 2) twoCounter++;
+    }
+    let arrayIterator: number = 0;
+    for(let i: number = 0;i < zeroCounter; i++) {
+        nums[arrayIterator] = 0;
+        arrayIterator++;
+    }
+    for(let i: number = 0;i < oneCounter; i++) {
+        nums[arrayIterator] = 1;
+        arrayIterator++;
+    }
+    for(let i: number = 0;i < twoCounter; i++) {
+        nums[arrayIterator] = 2;
+        arrayIterator++;
+    }
+};
