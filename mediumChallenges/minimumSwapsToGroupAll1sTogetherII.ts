e51f900d195b01d8d087153ed0db4b99b222453e
Yan Victor Brito Gomes <ybritogomes@gmail.com>
2024-08-03 12:56:16 -0300

    feat(dailyChallenge): Added another solution to minimumSwapsToGroupAll1sTogetherII.ts(2134) challenge

--- mediumChallenges/minimumSwapsToGroupAll1sTogetherII.ts
+++ mediumChallenges/minimumSwapsToGroupAll1sTogetherII.ts
@@ -27,3 +27,31 @@ function minSwaps(nums: number[]): number {
 
 	return (res ?? 0);
 };
+
+// Second Approach - Prefix summing zero's count in doubled nums array and sliding window with one's count number width getting the lowest number of zero's - (114ms - Beats 12.5%)
+function minSwaps(nums: number[]): number {
+	const n = nums.length;
+	nums.push(...nums);
+	
+	let prefixSumZeros: number[] = [0];
+	let oneCount = nums[0];
+	for(let i = 0; i < nums.length; i++) {
+		if(nums[i] === 1) oneCount++;
+		prefixSumZeros[i + 1] = prefixSumZeros[i] + (nums[i] === 0 ? 1 : 0);
+	}
+
+	oneCount = Math.floor(oneCount / 2);
+
+	let res: number = prefixSumZeros[oneCount] - prefixSumZeros[0];
+
+	let i = 1;
+	for(let j = oneCount; i < n; j++) {
+		const zeroCount = prefixSumZeros[j + 1] - prefixSumZeros[i];
+
+		res = Math.min(res, zeroCount);
+
+		i++;
+	}
+
+	return res;
+};
