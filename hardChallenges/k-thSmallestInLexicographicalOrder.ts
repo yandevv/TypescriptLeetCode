f929a132381fac1f04191e9130747dc53a19436e
YanDevv - LaptopIB <ybritogomes@gmail.com>
2025-06-09 08:37:42 -0300

    refactor(dailyChallenge): Refactored k-thSmallestInLexicographicalOrder.ts(440) challenge as it was today's daily challenge

--- hardChallenges/k-thSmallestInLexicographicalOrder.ts
+++ hardChallenges/k-thSmallestInLexicographicalOrder.ts
@@ -1,45 +1,64 @@
-// First Try - Same iterative algorithm from last submission but without previous numbers tracking and just returning by the index. (42 / 69 testcases passed - Time Limit Exceeded)
+// First Try - Same iterative algorithm from last submission but without previous numbers tracking and just
+// returning by the index. (42 / 69 testcases passed - Time Limit Exceeded)
 // function findKthNumber(n: number, k: number): number {
-// 	let x = 1;
-// 	for(let i = 0; i < n; i++) {
-// 		if(i === k - 1) break;
-// 		if(x * 10 > n) {
-// 			if(x === n) x = Math.floor(x / 10);
-// 			x++;
-// 			while(x % 10 === 0) x = Math.floor(x / 10);
-// 		} else x *= 10;
-// 	}
-// 	return x;
+//   let x = 1;
+
+//   for(let i = 0; i < k; i++) {
+//     if(i === k - 1)
+//       break;
+
+//     if(x * 10 > n) {
+//       if(x === n)
+//         x = Math.floor(x / 10);
+
+//       x++;
+
+//       while(x % 10 === 0)
+//         x = Math.floor(x / 10);
+//     } else
+//       x *= 10;
+//   }
+
+//   return x;
 // };
 
-// First Approach - Tracking the possible numbers count for each prefix and decrementing k by it's count if lesser than k, either decrementing once and incrementing current number. (https://leetcode.com/problems/k-th-smallest-in-lexicographical-order/solutions/5819085/beats-super-easy-beginners/?envType=daily-question&envId=2024-09-22)
+// First Approach - Tracking the possible numbers count for each prefix and decrementing k by it's count if
+// lesser than k, either decrementing once and incrementing current number (Editorial Solution).
 function countSteps(n: number, curr: number, next: number): number {
-	let steps = 0;
+  let steps = 0;
 
-	while(curr <= n) {
-		steps += Math.min(n + 1, next) - curr;
-		curr *= 10;
-		next *= 10;
-	}
+  while(curr <= n) {
+    steps += Math.min(n + 1, next) - curr;
 
-	return steps;
+    curr *= 10;
+    next *= 10;
+  }
+
+  return steps;
 }
 
 function findKthNumber(n: number, k: number): number {
-	let current = 1;
-	k--;
+  let current = 1;
 
-	while (k > 0) {
-		const count = countSteps(n, current, current + 1);
+  k--;
 
-		if(count <= k) {
-			current++;
-			k -= count;
-		} else {
-			current *= 10;
-			k--;
-		}
-	}
+  while(k > 0) {
+    const count = countSteps(n, current, current + 1);
 
-	return current;
+    if(count <= k) {
+      current++;
+      k -= count;
+    } else {
+      current *= 10;
+      k--;
+    }
+  }
+
+  return current;
 }
+
+const case1 = findKthNumber(13, 2);
+const case2 = findKthNumber(1, 1);
+
+console.log(`case1: ${case1} // ${case1 === 10}`);
+console.log(`case2: ${case2} // ${case2 === 1}`);
